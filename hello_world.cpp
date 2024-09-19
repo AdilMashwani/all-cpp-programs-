74dcae3526936c592bc7c6fa3d85dd96f38c5dab
Adil Mashwani <ak7175184@gmail.com>
2024-09-19 11:03:52 +0500

    Add files via upload
    This is the first coding program for the beginners who are new to the c++ language .
    I have described the basic topics in the program

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+#include<iostream>//--> #include is a preprocessor directive and iostream is a 
+                // header file use for input and output ?
+using namespace std;
+                //  |__> using namespace std means that you tell to the computer that 
+                //  include every standard library components .
+
+int main() //--> every program has a main() , because it's a kind of start of the program.
+{
+   cout<<"Hello, world ."; //--> here cout is the object of ostream(header file)."<<" is called
+                           // insertio operator . cout stands for "character output" .it is used
+                           //to convey your message to the console(screen).
+}
